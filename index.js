e078e7a5f6f37833e9a8d77b7a6ae5b01ba9dc98
amisdun <michealdunamis@gmail.com>
2022-04-25 14:15:19 +0000

    amisdun: minor changes made

---
+++ index.js
@@ -0,0 +1,16 @@
+const data = require("./index.json");
+
+const generateUpdateQuery = (data, table_name) => {
+  const getJsonKeys = Object.keys(data);
+  const getJsonValues = Object.values(data);
+  let updateString = "";
+  for (idx in getJsonKeys) {
+    updateString += `${getJsonKeys[idx]} = ${JSON.stringify(
+      getJsonValues[idx]
+    )}, `;
+  }
+  const updateQuery = `UPDATE ${table_name} SET ${updateString} WHERE id = 1`;
+  return updateQuery;
+};
+
+console.log(generateUpdateQuery(data, "users"));
